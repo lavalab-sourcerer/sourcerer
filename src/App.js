d99bbe51f2710e30f8ba2abb8b24bc5a10035bde
Woody Shin <woodyshi@usc.edu>
2021-11-14 00:38:58 -0800

    CRA starter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://cdn-icons-png.flaticon.com/512/234/234464.png" className="App-logo" alt="logo" />
+        <h1>sourcerer</h1>
+        <p>Creating magic ...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
